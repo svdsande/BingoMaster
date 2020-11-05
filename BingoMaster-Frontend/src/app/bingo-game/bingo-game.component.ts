bd68360f8ebcf792bad8de6276c22cab63ae2fca
Stijn van de Sande <stijnvandesande@ziggo.nl>
2020-11-05 23:14:28 +0100

    Started working on the game controls

--- BingoMaster-Frontend/src/app/bingo-game/bingo-game.component.ts
+++ BingoMaster-Frontend/src/app/bingo-game/bingo-game.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, Subscription, timer } from 'rxjs';
+import { repeatWhen, switchMap, take, takeUntil } from 'rxjs/operators';
 import { BingoGameModel } from 'src/api/api';
 import { BingoGameService } from '../services/bingo-game.service';
 
@@ -12,21 +14,65 @@ export class BingoGameComponent implements OnInit {
   public drawnNumbers: number[] = [];
   public bingoGame: BingoGameModel;
   public countDownDone: boolean = false;
+  public playing: boolean;
+  public timerDelay: number = 2000;
+  private start: Subject<void> = new Subject<void>();
+  private suspend: Subject<void> = new Subject<void>();
+  private subscription: Subscription = new Subscription();
 
   constructor(private bingoGameService: BingoGameService) { }
 
   ngOnInit(): void {
-    this.bingoGameService.nextRoundReceived.subscribe((model: BingoGameModel) => {
+    this.bingoGameService.nextRoundReceived
+    .pipe(take(1))
+    .subscribe((model: BingoGameModel) => {
       this.drawnNumbers.push(model.drawnNumber);
+      this.bingoGame.players = model.players;
     });
+
+    this.initiateTimer();
+    this.suspend.next();
   }
 
-  public playNextRound(): void {
-    this.bingoGameService.playNextRound(this.bingoGame.players, this.drawnNumbers);
+  public play(): void {
+    this.start.next();
+    this.playing = true;
+  }
+
+  public pause(): void {
+    this.suspend.next();
+    this.playing = false;
+  }
+
+  public stop(): void {
+
+  }
+
+  //TODO: Maybe switchMap so that timer could be restarted or updated
+  public rewind(): void {
+    this.timerDelay += 500;
+  }
+
+  public forward(): void {
+    this.timerDelay -= 500;
   }
 
   public onCountDownDone(): void {
     this.countDownDone = true;
     this.drawnNumbers.push(this.bingoGame.drawnNumber);
+  }
+
+  private initiateTimer(): void {
+    this.subscription.add(
+      timer(0, this.timerDelay).pipe(
+        switchMap(async () => this.playNextRound()),
+        takeUntil(this.suspend),
+        repeatWhen(() => this.start)
+      ).subscribe()
+    );
+  }
+
+  private playNextRound(): void {
+    this.bingoGameService.playNextRound(this.bingoGame.players, this.drawnNumbers);
   }
 }
