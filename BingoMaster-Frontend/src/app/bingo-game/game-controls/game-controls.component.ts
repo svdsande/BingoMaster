abd478813c01332669b340435b7c53567146c99f
Stijn van de Sande <stijnvandesande@ziggo.nl>
2020-11-09 21:46:24 +0100

    Forward en rewind timer

--- BingoMaster-Frontend/src/app/bingo-game/game-controls/game-controls.component.ts
+++ BingoMaster-Frontend/src/app/bingo-game/game-controls/game-controls.component.ts
@@ -19,12 +19,14 @@ export class GameControlsComponent implements OnInit, OnDestroy {
   public timerDelay: number = 2000;
   private start: Subject<void> = new Subject<void>();
   private suspend: Subject<void> = new Subject<void>();
+  private changeSpeed: Subject<number> = new Subject<number>();
   private subscription: Subscription = new Subscription();
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.initiateTimer();
+    this.changeSpeed.next(2000);
     this.suspend.next();
   }
 
@@ -56,18 +58,26 @@ export class GameControlsComponent implements OnInit, OnDestroy {
 
   public rewind(): void {
     this.timerDelay += 500;
+    this.changeSpeed.next(this.timerDelay);
   }
 
   public forward(): void {
     this.timerDelay -= 500;
+    this.changeSpeed.next(this.timerDelay);
   }
 
+  //TODO: Check if I can optimize this piece of code so that the subscription is only
+  //      fired in case the user clicks on the start button
   private initiateTimer(): void {
     this.subscription.add(
-      timer(0, this.timerDelay).pipe(
-        switchMap(async () => this.timerElapsed.emit()),
-        takeUntil(this.suspend),
-        repeatWhen(() => this.start)
+      this.changeSpeed.pipe(
+        switchMap((speed) =>
+          timer(0, speed).pipe(
+            switchMap(async () => this.timerElapsed.emit()),
+            takeUntil(this.suspend),
+            repeatWhen(() => this.start)
+          )
+        )
       ).subscribe()
     );
   }
