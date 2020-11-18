611d777ba8c50976e28cbf15247c0c7a9a345406
Stijn van de Sande <stijnvandesande@ziggo.nl>
2020-11-18 22:22:55 +0100

    Login form styling

--- BingoMaster-Frontend/src/app/authentication/login/login.component.ts
+++ BingoMaster-Frontend/src/app/authentication/login/login.component.ts
@@ -17,12 +17,13 @@ export class LoginComponent implements OnInit {
   }
 
   public authenticate(): void {
-
+    const email = this.loginFormGroup.get('email').value;
+    const password = this.loginFormGroup.get('password').value;
   }
 
   private buildForm(): void {
     this.loginFormGroup = new FormGroup({
-      username: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
       password: new FormControl('' ,Validators.required),
     });
   }
