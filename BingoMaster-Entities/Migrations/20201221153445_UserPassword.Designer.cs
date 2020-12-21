c290930a77e6ec21ebf83e5bc59512525b376c1a
Stijn van de Sande <stijnvandesande@ziggo.nl>
2020-12-21 16:38:56 +0100

    Database update

--- BingoMaster-Entities/Migrations/20201221153445_UserPassword.Designer.cs
+++ BingoMaster-Entities/Migrations/20201221153445_UserPassword.Designer.cs
@@ -4,14 +4,16 @@ using BingoMaster_Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BingoMaster_Entities.Migrations
 {
     [DbContext(typeof(BingoMasterDbContext))]
-    partial class BingoMasterDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201221153445_UserPassword")]
+    partial class UserPassword
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BingoMaster-Entities/Migrations/20201221153445_UserPassword.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BingoMaster_Entities.Migrations
+{
+    public partial class UserPassword : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "UserName");
+
+            migrationBuilder.RenameColumn(
+                name: "Lastname",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Firstname",
+                table: "Users",
+                newName: "FirstName");
+
+            migrationBuilder.RenameColumn(
+                name: "Emailaddress",
+                table: "Users",
+                newName: "EmailAddress");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailAddress",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Hash",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_EmailAddress",
+                table: "Users",
+                column: "EmailAddress",
+                unique: true,
+                filter: "[EmailAddress] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true,
+                filter: "[UserName] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_EmailAddress",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_UserName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Hash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "Lastname");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Users",
+                newName: "Firstname");
+
+            migrationBuilder.RenameColumn(
+                name: "EmailAddress",
+                table: "Users",
+                newName: "Emailaddress");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Emailaddress",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
