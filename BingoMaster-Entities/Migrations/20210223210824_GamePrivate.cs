dd5c89d004cbd6b5c825701d020912a75b196fed
Stijn van de Sande <stijnvandesande@ziggo.nl>
2021-02-23 22:09:31 +0100

    Game db migration

---
+++ BingoMaster-Entities/Migrations/20210223210824_GamePrivate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BingoMaster_Entities.Migrations
+{
+    public partial class GamePrivate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CenterSquareFree",
+                table: "Games",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Private",
+                table: "Games",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CenterSquareFree",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "Private",
+                table: "Games");
+        }
+    }
+}
