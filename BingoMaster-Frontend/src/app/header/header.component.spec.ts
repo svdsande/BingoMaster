b6c99a3e91542bb2da3fabb55b6cb8a13646a4b9
Stijn van de Sande <stijnvandesande@ziggo.nl>
2021-07-25 15:46:40 +0200

    Header component tests

--- BingoMaster-Frontend/src/app/header/header.component.spec.ts
+++ BingoMaster-Frontend/src/app/header/header.component.spec.ts
@@ -1,18 +1,30 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { AuthenticationService } from '../services/authentication/authentication.service';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
 
 import { HeaderComponent } from './header.component';
+
+let authenticationServiceSpy: jasmine.SpyObj<AuthenticationService>;
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(waitForAsync(() => {
+    const spy = jasmine.createSpyObj('AuthenticationService', [], ['currentUser']);
+
     TestBed.configureTestingModule({
       declarations: [ HeaderComponent ],
-      imports: [TranslateModule.forRoot()]
+      providers: [ { provide: AuthenticationService, useValue: spy } ],
+      imports: [TranslateModule.forRoot(), MatIconModule, MatIconTestingModule, RouterModule.forRoot([]) ]
     })
     .compileComponents();
+
+    authenticationServiceSpy = TestBed.inject(AuthenticationService) as jasmine.SpyObj<AuthenticationService>;
   }));
 
   beforeEach(() => {
@@ -23,5 +35,13 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show login section', () => {
+    spyOnProperty(authenticationServiceSpy, 'currentUser').and.returnValue(of(null));
+
+    const loginButtonElement = fixture.debugElement.nativeElement.getElementById('login-button');
+
+    expect(loginButtonElement).toBeTruthy();
   });
 });
