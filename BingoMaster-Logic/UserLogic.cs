d01ee4353635e580f1966d68d77f2acd85358278
Stijn van de Sande <stijnvandesande@ziggo.nl>
2022-01-20 13:27:44 +0100

    Abstract private methods

--- BingoMaster-Logic/UserLogic.cs
+++ BingoMaster-Logic/UserLogic.cs
@@ -12,129 +12,152 @@ using System.Linq;
 namespace BingoMaster_Logic
 {
     public class UserLogic : IUserLogic
-	{
-		#region Fields
+    {
+        #region Fields
 
-		private readonly BingoMasterDbContext _context;
-		private readonly IPasswordLogic _passwordLogic;
-		private readonly IPlayerLogic _playerLogic;
-		private readonly ITokenLogic _tokenLogic;
-		private readonly IMapper _mapper;
+        private readonly BingoMasterDbContext _context;
+        private readonly IPasswordLogic _passwordLogic;
+        private readonly IPlayerLogic _playerLogic;
+        private readonly ITokenLogic _tokenLogic;
+        private readonly IMapper _mapper;
 
-		#endregion
+        #endregion
 
-		public UserLogic(BingoMasterDbContext context, IPasswordLogic passwordLogic, IPlayerLogic playerLogic, ITokenLogic tokenLogic, IMapper mapper)
-		{
-			_context = context;
-			_passwordLogic = passwordLogic;
-			_playerLogic = playerLogic;
-			_tokenLogic = tokenLogic;
-			_mapper = mapper;
-		}
+        public UserLogic(BingoMasterDbContext context, IPasswordLogic passwordLogic, IPlayerLogic playerLogic, ITokenLogic tokenLogic, IMapper mapper)
+        {
+            _context = context;
+            _passwordLogic = passwordLogic;
+            _playerLogic = playerLogic;
+            _tokenLogic = tokenLogic;
+            _mapper = mapper;
+        }
 
-		public AuthenticatedUserModel Authenticate(AuthenticateUserModel authenticateUserModel)
-		{
-			if (authenticateUserModel == null || string.IsNullOrWhiteSpace(authenticateUserModel.EmailAddress) || string.IsNullOrWhiteSpace(authenticateUserModel.Password))
-			{
-				throw new ArgumentException("No email address or password provided");
-			}
+        public AuthenticatedUserModel Authenticate(AuthenticateUserModel authenticateUserModel)
+        {
+            if (!MinimalRequiredAuthenticationInformationProvided(authenticateUserModel))
+            {
+                throw new ArgumentException("No email address or password provided");
+            }
 
-			var user = _context.Users
-				.Include(user => user.Player)
-				.FirstOrDefault(user => user.EmailAddress == authenticateUserModel.EmailAddress);
+            var user = GetUserByEmailAddress(authenticateUserModel.EmailAddress);
 
-			if (user == null)
-			{
-				return null;
-			}
+            if (user == null)
+            {
+                return null;
+            }
 
-			if (!_passwordLogic.VerifyPassword(authenticateUserModel.Password, user.Hash, user.Salt))
-			{
-				throw new Exception("Login failed");
-			}
+            if (!_passwordLogic.VerifyPassword(authenticateUserModel.Password, user.Hash, user.Salt))
+            {
+                throw new Exception("Login failed");
+            }
 
-			var model = _mapper.Map<AuthenticatedUserModel>(user);
-			model.Token = _tokenLogic.GenerateToken(user);
+            var model = _mapper.Map<AuthenticatedUserModel>(user);
+            model.Token = _tokenLogic.GenerateToken(user);
 
-			return model;
-		}
+            return model;
+        }
 
-		public UserModel GetUserById(Guid id)
-		{
-			var user = _context.Users.Find(id);
+        public UserModel GetUserById(Guid id)
+        {
+            var user = _context.Users.Find(id);
 
-			return _mapper.Map<UserModel>(user);
-		}
+            return _mapper.Map<UserModel>(user);
+        }
 
-		public UserModel Register(RegisterUserModel registerUserModel)
-		{
-			if (registerUserModel == null || string.IsNullOrWhiteSpace(registerUserModel.EmailAddress) || string.IsNullOrWhiteSpace(registerUserModel.Password) || string.IsNullOrWhiteSpace(registerUserModel.PlayerName))
-			{
-				throw new ArgumentException("No email address, playername or password provided");
-			}
+        public UserModel Register(RegisterUserModel registerUserModel)
+        {
+            if (!MinimalRequiredRegistrationInformationProvided(registerUserModel))
+            {
+                throw new ArgumentException("No email address, playername or password provided");
+            }
 
-			if (!_playerLogic.PlayerNameUnique(registerUserModel.PlayerName) || !EmailAddressUnique(registerUserModel.EmailAddress))
-			{
-				throw new UserAlreadyExistsException("Player or user already exists");
-			}
+            if (!_playerLogic.PlayerNameUnique(registerUserModel.PlayerName) || !EmailAddressUnique(registerUserModel.EmailAddress))
+            {
+                throw new UserAlreadyExistsException("Player or user already exists");
+            }
 
-			var passwordStrength = _passwordLogic.GetPasswordStrength(registerUserModel.Password);
+            var passwordStrength = _passwordLogic.GetPasswordStrength(registerUserModel.Password);
 
-			if (passwordStrength != PasswordStrength.Strong && passwordStrength != PasswordStrength.VeryStrong)
-			{
-				throw new ArgumentException("Provided password too weak");
-			}
+            if (passwordStrength != PasswordStrength.Strong && passwordStrength != PasswordStrength.VeryStrong)
+            {
+                throw new ArgumentException("Provided password too weak");
+            }
 
-			var salt = _passwordLogic.GetRandomSalt();
-			var hashedPassword = _passwordLogic.GetHashedPassword(registerUserModel.Password, salt);
+            var newUser = ConstructNewUserWithPassword(registerUserModel);
 
-			var newUser = new User
-			{
-				EmailAddress = registerUserModel.EmailAddress,
-				FirstName = registerUserModel.FirstName,
-				LastName = registerUserModel.LastName,
-				Salt = Convert.ToBase64String(salt),
-				Hash = hashedPassword,
-				Player = new Player
-				{
-					Name = registerUserModel.PlayerName
-				}
-			};
+            _context.Add(newUser);
+            _context.SaveChanges();
 
-			_context.Add(newUser);
-			_context.SaveChanges();
+            return _mapper.Map<UserModel>(newUser);
+        }
 
-			return _mapper.Map<UserModel>(newUser);
-		}
+        public bool EmailAddressUnique(string emailAddress)
+        {
+            if (string.IsNullOrWhiteSpace(emailAddress))
+            {
+                throw new ArgumentException("No email address provided");
+            }
 
-		public bool EmailAddressUnique(string emailAddress)
-		{
-			if (string.IsNullOrWhiteSpace(emailAddress))
-			{
-				throw new ArgumentException("No email address provided");
-			}
+            var user = _context.Users.FirstOrDefault(user => user.EmailAddress == emailAddress);
 
-			var user = _context.Users.FirstOrDefault(user => user.EmailAddress == emailAddress);
+            if (user != null)
+            {
+                return false;
+            }
 
-			if (user != null)
-			{
-				return false;
-			}
+            return true;
+        }
 
-			return true;
-		}
+        public void Update(UserModel userModel)
+        {
+            var user = _context.Users.FirstOrDefault(user => user.Id == userModel.Id);
 
-		public void Update(UserModel userModel)
-		{
-			var user = _context.Users.FirstOrDefault(user => user.Id == userModel.Id);
+            if (user == null)
+            {
+                throw new KeyNotFoundException("Entity does not exists");
+            }
 
-			if (user == null)
-			{
-				throw new KeyNotFoundException("Entity does not exists");
-			}
+            _context.Entry(user).CurrentValues.SetValues(userModel);
+            _context.SaveChanges();
+        }
 
-			_context.Entry(user).CurrentValues.SetValues(userModel);
-			_context.SaveChanges();
-		}
-	}
+        private User ConstructNewUserWithPassword(RegisterUserModel registerUserModel)
+        {
+            var salt = _passwordLogic.GetRandomSalt();
+            var hashedPassword = _passwordLogic.GetHashedPassword(registerUserModel.Password, salt);
+
+            return new User
+            {
+                EmailAddress = registerUserModel.EmailAddress,
+                FirstName = registerUserModel.FirstName,
+                LastName = registerUserModel.LastName,
+                Salt = Convert.ToBase64String(salt),
+                Hash = hashedPassword,
+                Player = new Player
+                {
+                    Name = registerUserModel.PlayerName
+                }
+            };
+        }
+
+        private bool MinimalRequiredRegistrationInformationProvided(RegisterUserModel registerUserModel)
+        {
+            return registerUserModel != null
+                && !string.IsNullOrWhiteSpace(registerUserModel.EmailAddress)
+                && !string.IsNullOrWhiteSpace(registerUserModel.Password)
+                && !string.IsNullOrWhiteSpace(registerUserModel.PlayerName);
+        }
+
+        private bool MinimalRequiredAuthenticationInformationProvided(AuthenticateUserModel authenticateUserModel)
+        {
+            return authenticateUserModel != null && !string.IsNullOrWhiteSpace(authenticateUserModel.EmailAddress) && !string.IsNullOrWhiteSpace(authenticateUserModel.Password);
+        }
+
+        private User GetUserByEmailAddress(string emailAddress)
+        {
+            return _context.Users
+                .Include(user => user.Player)
+                .FirstOrDefault(user => user.EmailAddress == emailAddress);
+        }
+    }
 }
